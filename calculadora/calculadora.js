306f3f116ec060cb4c01cc134a0624518e1a4a56
gabybeautiful <by.soares2005@gmail.com>
2026-02-25 16:43:15 -0300

    Aplicativo de calculadora

---
+++ calculadora/calculadora.js
@@ -0,0 +1,33 @@
+function calculadora (){
+    while(true) 
+    
+        let resultado = 0.0;
+        let operando1;
+        let operando2;
+        let operador;
+
+        operando = parseFloat( prompt("Digite o primeiro número") );
+        operando = parseFloat( prompt("Digite o segundo número:") );
+        operador = prompt("Digite uma das operações (+ - * / ):");
+
+        if (operador === "+"){
+            resultado = operando1 + operando2;
+        } else if ( operador === "-" ){
+            resultado = operando1 - operando2;
+        } else if ( operador === "*" ){
+            resultado = operando1 * operando2;
+        } else if (operador === "/"){  
+            if (operando2 == 0){
+                alert("Não é possível dividir por zero.")
+                return
+            }
+            resultado = operando1 / operando2;
+        } else { 
+            alert("Operador inválido!");
+            return
+        } 
+
+        alert("Resultado: " +
+ }           operando1 + " " + operador + " " + operando2 + " = " + resultado);
+
+     
