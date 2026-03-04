762ebb1757ca69721583cf089f282efc69fe19dc
gabybeautiful <by.soares2005@gmail.com>
2026-03-04 16:55:08 -0300

    feat: opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,13 +1,15 @@
-function calculadora (){
-    while(true) 
+function calculadora () {
+    while(true) {
     
         let resultado = 0.0;
         let operando1;
         let operando2;
         let operador;
+        let continua;
+        let operador_valido = true;
 
-        operando = parseFloat( prompt("Digite o primeiro número") );
-        operando = parseFloat( prompt("Digite o segundo número:") );
+        operando1 = parseFloat( prompt("Digite o primeiro número") );
+        operando2 = parseFloat( prompt("Digite o segundo número:") );
         operador = prompt("Digite uma das operações (+ - * / ):");
 
         if (operador === "+"){
@@ -18,16 +20,34 @@ function calculadora (){
             resultado = operando1 * operando2;
         } else if (operador === "/"){  
             if (operando2 == 0){
-                alert("Não é possível dividir por zero.")
-                return
+                alert("Não é possível dividir por zero.") 
+            
+        
+            continua = prompt ("digite sim para continuar e não para encerrar.");
+                if (continua === "não"){
+                   return 
+                } 
+
+            } else {
+                resultado = operando1 / operando2;
             }
-            resultado = operando1 / operando2;
+                
+            
+            
         } else { 
             alert("Operador inválido!");
-            return
-        } 
+            operador_valido = false;
+            continua = prompt ("digite sim para continuar e não para encerrar.");
+                if (continua === "não"){
+                   return 
+            
+                } 
+            }
+            if(operador != "/" || operando2 != 0 ){
+  
+         alert ("Resultado: " +
+         operando1 + " " + operador + " " + operando2 + " = " + resultado) ;
+        }
+    }   
 
-        alert("Resultado: " +
- }           operando1 + " " + operador + " " + operando2 + " = " + resultado);
-
-     
+}       
