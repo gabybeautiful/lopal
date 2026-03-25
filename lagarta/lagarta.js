7bd2d9b75bbbc15ebf87c5a70bd546e11ace04b3
gabybeautiful <by.soares2005@gmail.com>
2026-03-25 16:49:14 -0300

    feat:switch, ternário, while, for e lagarta

---
+++ lagarta/lagarta.js
@@ -0,0 +1,16 @@
+async function lagarta(){
+    let lagarta = "()()()()(00)";
+    let ampliar = "( )( )( )( )(00)";
+    let espaco = " ";
+
+    function sleep(ms){
+        return new Promise( resolve => setTimeout(resolve, ms));
+    }
+    
+    for(let i = 0; i < 20; i++){
+        console.log(i % 2 == 0? lagarta = espaco + lagarta:
+            ampliar = espaco + ampliar);
+        await sleep (500);
+        console.clear();
+    }
+}
---
+++ switch/switch.js
@@ -0,0 +1,22 @@
+function meuSwitch(){
+
+    let dia;
+    let data = new Date().getDay();
+
+    switch(data){
+        case 0:
+            dia = "Domingo";
+            break;
+        case 2:
+        case 3:
+        case 4:
+            dia = "o meio da semana"
+            break;
+        case 6: 
+            dia = "Sabádo";
+            break;
+            default:
+                dia = "segunda ou sexta"
+    } 
+    document.getElementById("demo").innerHTML ="Hoje é " + dia;
+}
